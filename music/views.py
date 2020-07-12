a6c387dd71539eb096097cf3a8acf2d744631e0a
NimaAram <aramnima50@gmail.com>
2020-07-12 15:51:56 +0430

    auth : permissions added to project

--- music/views.py
+++ music/views.py
@@ -1,8 +1,9 @@
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view , permission_classes
 from .models import Music
 from .serializers import MusicSerializers
 from rest_framework import status
+from rest_framework.permissions import IsAdminUser , AllowAny
 # @api_view()
 @api_view(['GET','POST'])
 def capture(request):
@@ -13,6 +14,7 @@ def capture(request):
         return Response({'error':"1000"})
 
 @api_view()
+@permission_classes([AllowAny])
 def sign(request):
     mus = Music.objects.all()
     ser_data =  MusicSerializers(mus,many=True)
@@ -20,6 +22,7 @@ def sign(request):
 
 
 @api_view()
+@permission_classes([AllowAny])
 def signid(request,id):
     try:
         music = Music.objects.get(pk=id)
@@ -29,6 +32,7 @@ def signid(request,id):
     return Response(ser_data.data)
 
 @api_view(['POST'])
+@permission_classes([IsAdminUser])
 def relase(request):
     info = MusicSerializers(request.FILES,data=request.data)
     if info.is_valid():
--- musico/settings.py
+++ musico/settings.py
@@ -128,5 +128,9 @@ MEDIA_ROOT = os.path.join('media')
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES' : [
+        'rest_framework.permissions.IsAuthenticated',
+
     ]
 }
